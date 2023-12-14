bf01911a8392f5880dc2294c2d0587e2be882350
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-12-14 17:38:23 -0300

    feat: add Person Service

---
+++ src/main/java/com/devsuperior/aula/services/PersonService.java
@@ -0,0 +1,37 @@
+package com.devsuperior.aula.services;
+
+import com.devsuperior.aula.DTO.PersonDepartamentDTO;
+import com.devsuperior.aula.entities.Department;
+import com.devsuperior.aula.entities.Person;
+import com.devsuperior.aula.repositories.DepartmentRepository;
+import com.devsuperior.aula.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    final PersonRepository personRepository;
+    final DepartmentRepository departmentRepository;
+    @Autowired
+    public PersonService(PersonRepository personRepository, DepartmentRepository departmentRepository) {
+        this.personRepository = personRepository;
+        this.departmentRepository = departmentRepository;
+    }
+
+    public PersonDepartamentDTO insert(PersonDepartamentDTO personDepartamentDTO) {
+        Person person = new Person();
+
+        CopyPerson(person, personDepartamentDTO);
+        Department department = departmentRepository.getReferenceById(personDepartamentDTO.getDepartment().getId());
+
+        person.setDepartment(department);
+        person = personRepository.save(person);
+        return new PersonDepartamentDTO(person);
+    }
+
+    public void CopyPerson(Person person, PersonDepartamentDTO personDepartamentDTO) {
+        person.setName(personDepartamentDTO.getName());
+        person.setSalary(personDepartamentDTO.getSalary());
+    }
+}
